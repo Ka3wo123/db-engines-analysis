4bd03f4969082ebeb19091572e17c0757ec63449
Kajetan Nowak <kajtek2n@gmail.com>
2025-11-14 16:45:33 +0100

    Add sh loader for keyspace from env file

---
+++ schemas/nosql/cql/init_db.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+docker-entrypoint.sh cassandra &
+CASSANDRA_PID=$!
+
+echo "Waiting for Cassandra build up..."
+until cqlsh cassandra -e 'DESCRIBE KEYSPACES'; do
+    sleep 5
+done
+
+KEYSPACE=${CASSANDRA_KEYSPACE:-default_keyspace}
+
+sed "s|{{KEYSPACE}}|${KEYSPACE}|g" /cql-init/create_keyspace_and_transactions.cql.template > /schema.cql
+
+echo "Applying schema for keyspace: $KEYSPACE"
+cqlsh cassandra -f /schema.cql
+
+wait $CASSANDRA_PID
